78114fd620f79450e3f718fe3cd242f6e1afae92
CarlosClaudino <ferreiraclaudinocarlos@gmail.com>
2026-08-31 07:56:15 -0300

    Add scrollable view with sample text
    Initial implementation of a scrollable view with multiple text elements.

---
+++ scrollview.jsx
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View, ScrollView} from 'react-native';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+
+export default function App() {
+  return (
+    <SafeAreaProvider style={{ flex: 1 }} >
+      <SafeAreaView style={{ flex: 1 }} >
+        <ScrollView style={{ flex: 1 }} >
+          <Text style={{margin: 30}}> Teste! </Text>
+          <Text style={{margin: 30}}> Teste! </Text>
+          <Text style={{margin: 30}}> Teste! </Text>
+          <Text style={{margin: 30}}> Teste! </Text>
+          <Text style={{margin: 30}}> Teste! </Text>
+          <Text style={{margin: 30}}> Teste! </Text>
+          <Text style={{margin: 30}}> Teste! </Text>
+          <Text style={{margin: 30}}> Teste! </Text>
+          <Text style={{margin: 30}}> Teste! </Text>
+          <Text style={{margin: 30}}> Teste! </Text>
+          <Text style={{margin: 30}}> Teste! </Text>
+        </ScrollView>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
